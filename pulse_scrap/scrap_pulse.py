cb650c17bf70124a1403b1cf975477250df52c42
rufai <rufai.mustopha@gmail.com>
2017-10-16 14:16:37 +0100

    refactored the file scrap_pulse.ng

--- pulse_scrap/scrap_pulse.py
+++ pulse_scrap/scrap_pulse.py
@@ -1,40 +1,17 @@
 import urllib.request
 from bs4 import BeautifulSoup as bs
 
-# url_to_fetch = "http://www.pulse.ng/sports/manchester-united-liverpool-draw-0-0-id7460504.html"
-url_to_fetch = "http://www.pulse.ng/entertainment/celebrities/5-celebrities-who-died-just-before-their-prime-id7447346.html"
+# test urls - uncomment one url at a time 
+
+url_to_fetch = "http://www.pulse.ng/sports/manchester-united-liverpool-draw-0-0-id7460504.html"
+# url_to_fetch = "http://www.pulse.ng/entertainment/celebrities/5-celebrities-who-died-just-before-their-prime-id7447346.html"
 # url_to_fetch = "http://www.pulse.ng/sports/football/arsene-wenger-manager-admits-ozil-sanchez-could-leave-in-january-id7451069.html"
 # url_to_fetch = "http://www.pulse.ng/sports/football/barcelona-fc-what-next-for-football-club-in-case-of-catalan-independence-id7443368.html"
 # url_to_fetch = "http://www.pulse.ng/news/politics/femi-adesina-calls-ezekwesili-a-wailing-wailer-id7465827.html"
 # url_to_fetch = "http://www.pulse.ng/entertainment/celebrities/omotola-hubby-get-raunchy-in-pool-pics-id7463976.html"
 
-# query the website and return the html to the variable ‘page’
-page = urllib.request.urlopen(url_to_fetch).read()
-
-soup = bs(page, 'html.parser')
-
-head_line = soup.find_all('div', attrs={"class": "vspace"})
-# print(head_line.h2)
-# print(head_line.h3)
-# for x in range(0,len(head_line)):
-# 	print(head_line[x])
-# 	print()
-# 	print()
-# 	print()
-# 	print()
-	
-# print(len(head_line))
-# print(head_line[1])
-
-# print(soup.get_text())
-# print(page)
-
-# head_line
-# snippet
-# pic_url as a list
-# article
-
-
+# query the url, get the html to the variable ‘page’
+# returns Beautiful Soup Object
 
 def get_article(url):
 	url_to_fetch = url
@@ -47,7 +24,7 @@ def get_article(url):
 # returns the Headline of particular blog post
 # accepts beautiful soup object of the page in question
 
-def get_head_line(BeautifulSoupObject, url):
+def get_head_line(BeautifulSoupObject):
 	soup = BeautifulSoupObject
 	head_line = soup.find('div', attrs={"class",'vspace'})
 	return head_line.h2
@@ -56,7 +33,7 @@ def get_head_line(BeautifulSoupObject, url):
 # returns a list of images in a particular blog post
 # accepts beautiful soup object of the page in question
 
-def get_picture_urls(BeautifulSoupObject, url):
+def get_picture_urls(BeautifulSoupObject):
 	blog_images = []
 		
 	soup_obj = BeautifulSoupObject
@@ -94,6 +71,6 @@ def get_options_from_social_media():
 	pass
 
 
-# print(get_head_line(soup))
-# get_picture_urls(soup)
-get_article_snippet(soup)
+soup = get_article(url_to_fetch)
+get_picture_urls(soup)
+# get_article_snippet(soup)
