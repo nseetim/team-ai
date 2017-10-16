512ca9ec592d15b19d426459e21bbe753df2ba39
Etim Nse-Abasi <etimnseabasi@gmail.com>
2017-10-16 11:18:42 +0100

    added main.py
    Signed-off-by: Etim Nse-Abasi <etimnseabasi@gmail.com>

--- main.py
+++ main.py
@@ -14,3 +14,19 @@ NUMBER_OF_THREADS = int(input("Enter the number of threads \n(this is OS specifi
 
 queue = Queue()
 Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
+
+#Create worker threads (will die when main exits)
+
+# Each queued link is a new job
+def create_jobs():
+    for link in file_to_set(QUEUE_FILE):
+        queue.put(link)
+    queue.join()
+    crawl()
+
+# Check if there are items in the queue, if so, crawl them
+def crawl()
+    queued_links = file_to_set(QUEUE_FILE)
+    if len(queued_links)>0:
+        print(str(len(queued_links)) + ' links in the queue')
+        create_jobs()
